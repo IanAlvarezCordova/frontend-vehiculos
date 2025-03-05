add13a4684bbb3daaae05221db777ceae6d32829
Ian Joel <ijalvarez@espe.edu.ec>
2025-03-05 05:55:11 -0500

    cambio dist

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.06krl9ubtk"
+    "revision": "0.20f176vaqa"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
