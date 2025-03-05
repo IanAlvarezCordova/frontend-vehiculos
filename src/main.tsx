5016abe9e5e2648d2eb6c0db0cca73bccc7c4365
Ian Joel <ijalvarez@espe.edu.ec>
2025-03-05 05:26:25 -0500

    PWA arreglo

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { registerSW } from 'virtual:pwa-register';
 
 import 'primereact/resources/themes/lara-dark-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 import 'primeflex/primeflex.css';
 
-const updateSW = registerSW({
-  onNeedRefresh() {
-    console.log('Nueva versión disponible. Recarga la página para actualizar.');
-  },
-  onOfflineReady() {
-    console.log('La PWA está lista para usarse sin conexión.');
-  }
-});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- src/pages/Inicio.tsx
+++ src/pages/Inicio.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const Inicio: React.FC = () => {
     return (
         <div>
             <h1>Bienvenido a la aplicación de roles y usuarios</h1>
             <p>Utiliza el menú superior para navegar entre las distintas secciones</p>
+            <Link to="/login">Ir al login</Link>
         </div>
     );
 };
